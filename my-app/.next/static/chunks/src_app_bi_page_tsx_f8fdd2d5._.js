1b98953261558c9710f6c540bab853c8ff5a255c
s.h <shwindwalker@gmail.com>
2025-10-16 14:31:13 +0900

    remove .next

--- my-app/.next/static/chunks/src_app_bi_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b1a4f66a._.js",
-  "static/chunks/src_app_bi_app_7cdb6719.css"
-],
-    source: "dynamic"
-});
