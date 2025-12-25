98167bbd8e4945c351f64d40943f4493ce82a2e1
s.h <shwindwalker@gmail.com>
2025-12-25 14:32:43 +0900

    fix:responsive

--- my-app/src/app/real_estate/page.tsx
+++ my-app/src/app/real_estate/page.tsx
@@ -74,8 +74,10 @@ function App() {
       setChartData(data.population.data);
       setSum(data.population.sum);
       setRateData(data.population_rate);
+      const windowWidth:number=window.innerWidth<600?window.innerWidth:800;
+      console.log(windowWidth);
       const res2 = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tile2map?x=${data.x}&y=${data.y}&zoom_level=${data.zoom_level}`
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/tile2map?x=${data.x}&y=${data.y}&zoom_level=${data.zoom_level}&window_width=${windowWidth}`
       );
       const html = await res2.text();
   
@@ -90,36 +92,36 @@ function App() {
       <h1>Folium Map in React</h1>
       <div className='main-body'>
         <div className='map' dangerouslySetInnerHTML={{ __html: mapHtml }} />
-        <div>
-        <form onSubmit={handleSubmit} className="search-form">
-          <input
-            type="text"
-            placeholder="address"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="border rounded px-3 py-2"
-            required
-          />
-          <button type="submit">
-            🔎
-          </button>
-        </form>
-        {years.length > 0 && (
-          <div style={{ width: "600px", height: "400px" }}>
-            <PopulationStackedChart
-              years={years}
-              ages={ages}
-              data={chartData}
-              sum={sum}
+        <div className='popuration'>
+          <form onSubmit={handleSubmit} className="search-form">
+            <input
+              type="text"
+              placeholder="address"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              className="border rounded px-3 py-2"
+              required
             />
-          </div>
-          
-        )}
-        <div>
-            <PopulationComparison
-              population_rate={rateData}
-              population_sum={sum}
-            />
+            <button type="submit">
+              🔎
+            </button>
+          </form>
+          {years.length > 0 && (
+            <div className='stacked-chart'>
+              <PopulationStackedChart
+                years={years}
+                ages={ages}
+                data={chartData}
+                sum={sum}
+              />
+            </div>
+            
+          )}
+          <div>
+              <PopulationComparison
+                population_rate={rateData}
+                population_sum={sum}
+              />
           </div>
         </div>
       </div>
@@ -167,85 +169,83 @@ function PopulationStackedChart({ years, ages, data,sum }: Props) {
   );
 }
 function PopulationComparison({ population_rate, population_sum }: { population_rate: Rates, population_sum: number[] }){
-  console.log("popurate",population_rate.data);
+  
   if(!population_rate.data[0]){
     return <div>nainai</div>;
   }
   
-  console.log(population_rate.data[0]);
+  
   return (
     <div>
       <h2>人口比較（2025年基準）</h2>
       <div style={{display:'flex'}}>
-      <div style={{border:'solid',maxWidth:'100px',textAlign: 'center',padding: '5px',}}>
-        {population_rate.data[0][1]}
-        <div style={{border:'solid',textAlign: 'center',padding: '5px',}}>
-          {population_rate.data[1][1]}
-          <div style={{border:'solid',textAlign: 'center',padding: '5px',}}>
-            メッシュ
+        <div className='mesh-ex first-mesh-ex'>
+          {population_rate.data[0][1]}
+          <div className='mesh-ex'>
+            <p className='slash'>/</p>
+            {population_rate.data[1][1]}
+            <div className='mesh-ex'>
+              <p className='slash'>/</p>
+              メッシュ
+            </div>
           </div>
         </div>
+        <table className='comparison-table'>
+          <thead>
+            <tr>
+              {population_rate.year.slice(4,9).map((year,i)=>{
+                return(
+                  <th
+                    key={i}>
+                    {year}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>         
+              {population_rate.data[0].slice(4,9).map((popsum, i) => {
+                const growthFrom2025 = (Number(popsum) - Number(population_rate.data[0][4])) / Number(population_rate.data[0][4])*10; // 各年の伸び幅計算
+                const color = getColorForGrowth(growthFrom2025);
+                return (
+                  <td
+                    key={i}
+                    style={{
+                      backgroundColor: color,
+                    }}
+                  >
+                    {popsum.toLocaleString()}
+                    {makeInnerDiv(i,population_rate.data[1].slice(4,9),makeInnerDiv(i,population_sum.slice(0, 5),null))}
+                  </td>
+                );
+              })}
+            </tr> 
+          </tbody>
+        </table>
       </div>
-      <div>
-      <table>
-        <thead>
-          <tr>
-            {population_rate.year.slice(4,9).map((year,i)=>{
-              return(
-                <th
-                  key={i}>
-                  {year}
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>         
-            {population_rate.data[0].slice(4,9).map((popsum, i) => {
-              const growthFrom2025 = (Number(popsum) - Number(population_rate.data[0][4])) / Number(population_rate.data[0][4])*10; // 各年の伸び幅計算
-              const color = getColorForGrowth(growthFrom2025);
-              return (
-                <td
-                  key={i}
+        <table className='color-ex'>
+          <thead>
+            <tr>
+              <th colSpan={10}></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {[...Array(10)].map((_, i:number) => parseFloat(((i/10)-0.5).toFixed(1))).map((num,index)=>{
+                const color = getColorForGrowth(num);
+                return (
+                <td key={index}
                   style={{
                     backgroundColor: color,
                     textAlign: 'center',
-                    padding: '10px',
-                  }}
-                >
-                  {popsum.toLocaleString()}
-                  {makeInnerDiv(i,population_rate.data[1].slice(4,9),makeInnerDiv(i,population_sum.slice(0, 5),null))}
-                </td>
-              );
-            })}
-          </tr> 
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={10}></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {[...Array(10)].map((_, i:number) => parseFloat(((i/10)-0.5).toFixed(1))).map((num,index)=>{
-              const color = getColorForGrowth(num);
-              return (
-              <td key={index}
-                style={{
-                  backgroundColor: color,
-                  textAlign: 'center',
-                  padding: '10px',}}>
-                  {Math.round((num)*10)+"%"}
-              </td>);
-            })}
-          </tr>
-        </tbody>
-      </table>
-      </div>
-      </div>
+                    padding: '10px',}}>
+                    {Math.round((num)*10)+"%"}
+                </td>);
+              })}
+            </tr>
+          </tbody>
+        </table>
     </div>
   );
 }
@@ -259,13 +259,11 @@ const makeInnerDiv=(index:number,givenarr:(number|string)[],innerDiv:React.JSX.E
   //const color = `rgba(0, 149, 217, ${(growthFrom2025+0.5)})`;
   return(
     <div
+      className='inner-div'
       style={{
           backgroundColor: color,
-          textAlign: 'center',
-          padding: '10px',
-          border: 'solid',
         }}
-    >
+    ><br className='slash'/>
      {popuarr[index].toLocaleString()} 
      {innerDiv}    
     </div>
